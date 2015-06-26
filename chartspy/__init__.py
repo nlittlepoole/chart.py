7d62cb2a42d000db8d6b5cdf4edfb5030c567a94
Niger Little-Poole <nigerlittlepoole@tumblr.com>
2015-06-26 15:05:34 -0400

    Added installation stuff

---
+++ chartspy/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['LineChart', 'StackedChart',]
+
+
+from line import LineChart
+from stacked import StackedChart
--- settings.py
+++
@@ -1,6 +0,0 @@
-
-CANVAS ="""
-           <canvas id="myChart" width="400" height="400"></canvas>
-           <script>
-           var ctx = document.getElementById("myChart").getContext("2d");
-		"""
